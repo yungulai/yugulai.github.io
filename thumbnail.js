4ce38f664bfb007bb652454414b6755a640a13aa
yungulai <85999986+yungulai@users.noreply.github.com>
2021-06-17 23:06:16 +0800

    Create thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,11 +1,10 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    alert("Hello JavaScript");
     
     let targetImage = document.querySelector("#smart-image");
     
     targetImage.addEventListener("click", function() {
-    alert("点击了图片");
-});
+    alert(targetImage.classList.remove("small"););
+    });
     
 });
