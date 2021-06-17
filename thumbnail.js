1f758af202f673364df10e0f39357198a8ddd57d
yungulai <85999986+yungulai@users.noreply.github.com>
2021-06-17 23:26:28 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -10,9 +10,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
         targetImage.classList.add("small");
     }
     });
-    function iframeSize() {    var iframe = document.getElementsByTagName("iframe");    
-                           var num = iframe.length;    for (; num > 0; num--) {        iframe[num - 1].style.width = "100%";        
-                            iframe[num - 1].style.height = iframe[num - 1].scrollWidth * 0.5625 + "px";    }
-                          }
-    iframeSize();window.onresize = function() {    iframeSize();}
+    function iframeSize() {    
+        var iframe = document.getElementsByTagName("iframe");    
+        var num = iframe.length;    
+        for (; num > 0; num--) {        
+            iframe[num - 1].style.width = "100%";        
+            iframe[num - 1].style.height = iframe[num - 1].scrollWidth * 0.5625 + "px";    
+        }
+    }
+    iframeSize();window.onresize = function() {    
+        iframeSize();
+    }
 });
