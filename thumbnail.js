fff46716ead0868bfd3532c8904d8256f33c179d
yungulai <85999986+yungulai@users.noreply.github.com>
2021-06-17 22:55:38 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -2,4 +2,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     alert("Hello JavaScript");
     let targetImage = document.querySelector("#smart-image");
+    targetImage.addEventListener("click", function() {
+    alert("点击了图片");
+});
 });
