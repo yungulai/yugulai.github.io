fae998e1dc9e87ac172ed486c210d76a7e785ec7
yungulai <85999986+yungulai@users.noreply.github.com>
2021-06-19 15:31:37 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,15 +1,16 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
+
+    
     
     let targetImage = document.querySelector("#smart-image");
-    
+
     targetImage.addEventListener("click", function() {
     if (targetImage.classList.contains("small")) {
         targetImage.classList.remove("small");
-    } 
-        else {
+    } else {
         targetImage.classList.add("small");
     }
-    }};
-    
-    
+    });
+
+});
